226521ffa0c101741e01f663ae643b2947022e30
Patrick S <patrickceg@outlook.com>
2022-04-18 19:31:17 -0400

    Put the code between the dotted lines

--- server.js
+++ server.js
@@ -10,6 +10,8 @@ const myPlaintextPassword = 'sUperpassw0rd!';
 const someOtherPlaintextPassword = 'pass123';
 
 
+//START_ASYNC -do not remove notes, place code between correct pair of notes.
+
 bcrypt.hash(myPlaintextPassword, saltRounds, (err, hash) => {
     console.log(hash);
     //$2b$13$odQNDM1srvhQ14rTj.06beQN/sEoblb0rbh0xxzQzjhXzYzX9/9K2
@@ -17,11 +19,6 @@ bcrypt.hash(myPlaintextPassword, saltRounds, (err, hash) => {
         console.log(res); //true
     });
 });
-
-
-//START_ASYNC -do not remove notes, place code between correct pair of notes.
-
-
 
 //END_ASYNC
 
