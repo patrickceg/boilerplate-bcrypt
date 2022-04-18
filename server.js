eef01d6f944d0be5f33f14cbe3f1d88ed110f0c9
Patrick S <patrickceg@outlook.com>
2022-04-18 19:33:50 -0400

    Add sync copy-paste

--- server.js
+++ server.js
@@ -24,7 +24,10 @@ bcrypt.hash(myPlaintextPassword, saltRounds, (err, hash) => {
 
 //START_SYNC
 
-
+var hash = bcrypt.hashSync(myPlaintextPassword, saltRounds);
+console.log(hash)
+var result = bcrypt.compareSync(myPlaintextPassword, hash);
+console.log(result)
 
 //END_SYNC
 
