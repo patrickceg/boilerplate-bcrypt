53bc12bf5659c1e459f5b0eba397b10861636bb8
Patrick S <patrickceg@outlook.com>
2022-04-18 19:22:10 -0400

    Copy-paste in the hashing code

--- server.js
+++ server.js
@@ -10,6 +10,15 @@ const myPlaintextPassword = 'sUperpassw0rd!';
 const someOtherPlaintextPassword = 'pass123';
 
 
+bcrypt.hash('passw0rd!', 13, (err, hash) => {
+    console.log(hash);
+    //$2a$12$Y.PHPE15wR25qrrtgGkiYe2sXo98cjuMCG1YwSI5rJW1DSJp0gEYS
+    bcrypt.compare(someOtherPlaintextPassword, hash, (err, res) => {
+        console.log(res); //true
+    });
+});
+
+
 //START_ASYNC -do not remove notes, place code between correct pair of notes.
 
 
